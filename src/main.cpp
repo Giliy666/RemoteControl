356bd98ba6c519f5ec95aab6cf41acb5030d2667
Giliy666 <80615459+Giliy666@users.noreply.github.com>
2021-11-30 17:23:37 +0300

    Полная переделка
    Переделка под управление одновременно и мышью и клавиатурой

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,7 @@
-#include <map>
 #include <Arduino.h>
 #include <FreeRTOS.h>
-#include <Preferences.h>
-#define USE_NIMBLE
-#include <BleKeyboard.h>
-#include <Preferences.h>
-#include <WiFi.h>
+#include <BleCombo.h>
+
 
 #define BAT_PIN  33
 
@@ -15,40 +11,28 @@ int bat_proc =50;
 #define TOUTCH_PIN2 T5
 #define TOUTCH_PIN3 T6
 #define TOUTCH_PIN4 T7
-int touch_value_1 = 0;
-int touch_value_2 = 0;
-int touch_value_3 = 0;
-int touch_value_4 = 0;
+#define TOUTCH_PIN_CLICK T8
+
+int touch_value_click = 0;
+
+int threshold = 30;
 
 bool tach_stat [4]={0,0,0,0};
+int tach [4]={0,0,0,0};
 
-MediaKeyReport comand [14]=
-{
-0xB5,0xB6,0xCD,0xE9,0xEA,0xB7,0xC2, //Next Track,Previous Track,(Play/Pause),(Volume Increment),(Volume Decrement),(Stop)F1,
-0xD8,0xDA,0xD9,0xD7,//под замену
-0x80,0x81,0x82, //веремнно для тестов
-};
+bool work_mouse=0;
+bool work_key=0;
 
 int u=0;
 int u_sum=0;
-int sum_array=0000;
+int sum_array=00000;
 
+int count=0;
+//TaskHandle_t Task1;
+//TaskHandle_t Task2;
 
-Preferences preferences;
-
-TaskHandle_t Task1;
-TaskHandle_t Task2;
-
-const char* ssid     = "MALOMU";
-const char* password = "1";
-WiFiServer server(80);
-char lineBuf[80];
-int charCount = 0;
-
-BleKeyboard bleKeyboard("MALOMU","Samopal",bat_proc);
-
-void Task1code( void * pvParameters );
-void Task2code( void * pvParameters );
+//void Task1code( void * pvParameters );
+//void Task2code( void * pvParameters );
 
 int read_sens(uint8_t pin)
 {
@@ -62,180 +46,33 @@ int read_sens(uint8_t pin)
   return(u);
 }
 
+void sort()
+{
+  
+  
+}
+
 void setup() 
 {
 Serial.begin(115200);
 Serial.println("Starting BLE work!");
 
-preferences.begin("tehnolog-cat", false);
-preferences.clear();
-
-bleKeyboard.begin();
-
-WiFi.softAP(ssid, password);
-
-Serial.println();
-Serial.print("IP address: ");
-Serial.println(WiFi.softAPIP());
-
-xTaskCreatePinnedToCore(Task1code,"Task1",10000,NULL,1,&Task1,0); 
-  delay(5); 
-xTaskCreatePinnedToCore(Task2code,"Task2",10000,NULL,1,&Task2,1); 
-  delay(5);   
+Keyboard.begin();
+Mouse.begin();
 }
+
 void loop() 
 {
+  tach[0]=read_sens(TOUTCH_PIN1);
+  tach[1]=read_sens(TOUTCH_PIN2);
+  tach[2]=read_sens(TOUTCH_PIN3);
+  tach[3]=read_sens(TOUTCH_PIN4);
+
+  touch_value_click=read_sens(TOUTCH_PIN_CLICK);
   
-}
-void Task1code( void * pvParameters )
-{
-  while (1)
-  {
-    touch_value_1 = read_sens(TOUTCH_PIN1);       
-    touch_value_2 = read_sens(TOUTCH_PIN2); 
-    touch_value_3 = read_sens(TOUTCH_PIN3);    
-    touch_value_4 = read_sens(TOUTCH_PIN4);  
-
-    if (touch_value_1<30)
-    {
-      tach_stat[0]=1;
-    }
-    else
-    {
-      tach_stat[0]=0;
-    }
-    if (touch_value_2<30)
-    {
-      tach_stat[1]=1;
-    }
-    else
-    {
-      tach_stat[1]=0;
-    }
-    if (touch_value_3<30)
-    {
-      tach_stat[2]=1;
-    }
-    else
-    {
-      tach_stat[2]=0;
-    }
-    if (touch_value_4<30)
-    {
-      tach_stat[3]=1;
-    }
-    else
-    {
-      tach_stat[3]=0;
-    }
-    sum_array=(tach_stat[0]*1000)+(tach_stat[1]*100)+(tach_stat[2]*10)+(tach_stat[3]*1);
-    
-    Serial.print(sum_array);
-    
-    Serial.print('\n');    
-
-    switch (sum_array)
-    {
-      case 1:
-      bleKeyboard.write(comand[0]);
-      break;
-      case 10:
-      bleKeyboard.write(comand[1]);
-      break;
-      case 11:
-      bleKeyboard.write(comand[2]);
-      break;
-      case 100:
-      bleKeyboard.write(comand[3]);
-      break;
-      case 101:
-      bleKeyboard.write(comand[4]);
-      break;
-      case 110:
-      bleKeyboard.write(comand[5]);
-      break;
-      case 111:
-      bleKeyboard.write(comand[6]);
-      break;
-      case 1000:
-      bleKeyboard.write(comand[7]);
-      break;
-      case 1001:
-      bleKeyboard.write(comand[8]);
-      break;
-      case 1010:
-      bleKeyboard.write(comand[9]);
-      break;
-      case 1011:
-      bleKeyboard.write(comand[10]);
-      break;
-      case 1100:
-      bleKeyboard.write(comand[11]);
-      break;
-      case 1101:
-      bleKeyboard.write(comand[12]);
-      break;
-      case 1110:
-      bleKeyboard.write(comand[13]);
-      break;
-      case 1111:
-      bleKeyboard.write(comand[14]);
-      break;    
-    default:
-      delay(100);
-      break;
-    }    
-
-  } 
-  
+  Keyboard.println("Connect");
+  Keyboard.println(count);
+  count++;
+  delay(5000);
 }
 
-void Task2code( void * pvParameters )
-{ 
-  while (1)
-  {  
-    WiFiClient client = server.available(); // прослушка входящих клиентов
-  if (client) {
-    Serial.println("New client");
-    memset(lineBuf, 0, sizeof(lineBuf));
-    charCount = 0;
-    // HTTP-запрос заканчивается пустой строкой
-    boolean currentLineIsBlank = true;
-    while (client.connected()) {
-      client.println("HTTP/1.1 200 OK");
-      client.println("Content-Type: text/html");
-      client.println("Connection: close");
-      client.println();
-	  
-      // формируем веб-страницу
-      String webPage = "<!DOCTYPE HTML>";
-      webPage += "<html>";
-      webPage += "<head>";
-      webPage += "<meta name=\"viewport\" content=\"width=device-width,";
-      webPage += "initial-scale=1\">";
-      webPage += "</head>";
-      webPage += "<body>";
-      webPage += "<h1>ESP32 - Web Server</h1>";
-      webPage += "<p>AnalogPin 34 = ";
-      webPage +=    analogRead(34);
-      webPage += "</p>";
-      webPage += "<p>AnalogPin 36 = ";
-      webPage +=    analogRead(36);
-      webPage += "</p>";
-      webPage += "<p>AnalogPin 39 = ";
-      webPage +=    analogRead(39);
-      webPage += "</p>";
-      webPage += "</body></html>";
-      client.println(webPage);
-      break;
-    }
-    // Даём веб-браузеру время для получения данных
-    delay(1);
-    // Закрываем соединение
-    client.stop();
-    Serial.println("client disconnected");
-  }
-  }
-  
- 
-}
